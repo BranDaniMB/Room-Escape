c3bca9cff702e330eac534772c7e6818a732e529
Jermy <jermydavidcalvo@gmail.com>
2018-06-11 11:07:25 -0600

    new commit

--- src/files/Test.java
+++ src/files/Test.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package files;
 
 import game.Chrono;
@@ -20,7 +15,7 @@ public class Test {
         // TODO code application logic here
         PropertiesConfig.getInstance().readConfig();
         PropertiesConfig.getInstance().addConfig("Num", "65");
-//        new Chrono().start();
+        new Chrono().start();
     }
     
 }
