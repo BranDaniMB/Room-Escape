4a3bc1abca23f5ad9ecd6c4f0dad86f28be0139e
Jermy <jermydavidcalvo@gmail.com>
2018-06-11 14:44:04 -0600

    new

--- src/game/Game.java
+++ src/game/Game.java
@@ -9,6 +9,7 @@ import builderteam.InvalidDataException;
 import files.PropertiesConfig;
 import java.util.ArrayList;
 import listManager.TeamList;
+import objects.Player;
 import objects.Team;
 
 /**
@@ -16,23 +17,46 @@ import objects.Team;
  * @author Jermy
  */
 public class Game extends Thread {
-
+    
     private Chrono chrono;
     private ArrayList<GameRoom> gameRooms;
     private boolean finishGame;
-
+    
     public synchronized void singularMode() throws InterruptedException {
         while (true) {
             wait();
         }
     }
-
-    public synchronized void selectPlayer(String name) {
-
+    
+    public synchronized void selectToPlay(String TeamName, String players) throws InvalidDataException {
+        Team aux = null;
+        selectTeam(TeamName, aux);
+        String[] player = players.split("-");
+        if (player.length == 1) {
+            throw new InvalidDataException("Debe contener al menos 2 jugadores");
+        }
+        for (int i = 0; i < player.length; i++) {
+            selectPlayer(player[i].toString(), aux);
+        }
     }
-
-    public synchronized void selectTeam(String name) throws InvalidDataException {
-        Team aux = TeamList.getInstance().searchTeam(name);
+    
+    private synchronized void selectPlayer(String name, Team team) throws InvalidDataException {
+        Player player = team.searchPlayer(name);
+        if (player.isSelected()) {
+            throw new InvalidDataException("Jugador ya seleccionado\n Eliga diferentes jugadores");
+        }
+        if ((team.getPlayersOnline() == PropertiesConfig.getInstance().getProperties("maxPlayers")) && !player.isSelected()) {
+            throw new InvalidDataException("Ya se encuentran los jugadores completos");
+        }
+        if (player == null) {
+            throw new InvalidDataException("Jugador No existe");
+        }
+        player.setSelected(true);
+        team.setPlayersOnline(team.getPlayersOnline() + 1);
+    }
+    
+    private synchronized void selectTeam(String name, Team aux) throws InvalidDataException {
+        aux = TeamList.getInstance().searchTeam(name);
         if (aux == null) {
             throw new InvalidDataException("Equipo Inexistente");
         }
@@ -42,45 +66,45 @@ public class Game extends Thread {
         aux.setPlaying(true);
         TeamList.getInstance().setTeamsPlaying(TeamList.getInstance().getTeamsPlaying() + 1);
     }
-
+    
     public synchronized void multiplayerMode() throws InterruptedException {
         while (true) {
             wait();
         }
-
+        
     }
-
+    
     public void starPlay() {
     }
-
+    
     public void createGameRoom() {
         GameRoom gameRoom = new GameRoom(this);
         gameRooms.add(gameRoom);
         gameRoom.start();
     }
-
+    
     public Chrono getChrono() {
         return chrono;
     }
-
+    
     public void setChrono(Chrono chrono) {
         this.chrono = chrono;
     }
-
+    
     public ArrayList<GameRoom> getGameRooms() {
         return gameRooms;
     }
-
+    
     public void setGameRooms(ArrayList<GameRoom> gameRooms) {
         this.gameRooms = gameRooms;
     }
-
+    
     public boolean isFinishGame() {
         return finishGame;
     }
-
+    
     public void setFinishGame(boolean finishGame) {
         this.finishGame = finishGame;
     }
-
+    
 }
