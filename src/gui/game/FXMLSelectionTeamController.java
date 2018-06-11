8c4c7a968e2111cb685e1aaa0669aeef510da25e
Brandon Daniel <bmb24486134@gmail.com>
2018-06-11 11:55:44 -0600

    new commit
    add new packages and classes

---
+++ src/gui/game/FXMLSelectionTeamController.java
@@ -0,0 +1,22 @@
+package gui.game;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author BranDaniMB
+ */
+public class FXMLSelectionTeamController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
