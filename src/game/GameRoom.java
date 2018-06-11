6981ff968e63a0586f1d9ec3ce46a14dcf559434
Brandon Daniel <bmb24486134@gmail.com>
2018-06-11 10:50:06 -0600

    new commit

--- src/game/GameRoom.java
+++ src/game/GameRoom.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package game;
 
-import Objects.Riddle;
+import objects.Riddle;
 import files.PropertiesConfig;
 import gui.rooms.ManagerGameWindows;
 import java.util.ArrayList;
