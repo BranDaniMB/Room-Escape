4976620993835098556682aa8f86de9b418f4721
Jermy <jermydavidcalvo@gmail.com>
2018-06-17 08:38:13 -0600

    new

--- src/game/GameRoom.java
+++ src/game/GameRoom.java
@@ -20,10 +20,10 @@ import objects.Team;
  * @author Jermy
  */
 public class GameRoom extends Thread implements Subject {
-    
+
     public static final String TYPE_GAME_SINGLE = "single";
     public static final String TYPE_GAME_MULTIPLAYER = "multiplayer";
-    
+
     private Game game;
     private Team team;
     private ArrayList<Padlock> padlocks;
@@ -32,11 +32,11 @@ public class GameRoom extends Thread implements Subject {
     private String type;
     private Subteam subteam;
     private RoomInterface room;
-    
+
     public boolean isSingleGame() {
         return type.equals("single");
     }
-    
+
     public GameRoom(Game game, Team team, Subteam subteam, RoomRiddle gameRiddle, String type) {
         this.game = game;
         this.padlocks = new ArrayList<>();
@@ -49,7 +49,7 @@ public class GameRoom extends Thread implements Subject {
         loadPadlocks(gameRiddle);
         team.setSelect(false);
     }
-    
+
     public GameRoom(Game game, Team team, RoomRiddle gameRiddle, String type) {
         this.game = game;
         this.padlocks = new ArrayList<>();
@@ -78,62 +78,60 @@ public class GameRoom extends Thread implements Subject {
             throw new InvalidDataException("Perdió");
         }
     }
-    
+
     public void tryUnlockPadlock(String msj, int padlock) {
         if (padlocks.get(padlock).tryOpen((msj.toLowerCase().trim()))) {
             unlock++;
             updateInfo(padlocks.get(padlock).getIdPadlock() + " desbloqueado\n");
             updatePadlock(padlock);
-            setPadlockText();
+            setPadlockText(padlock);
             if (unlock == PropertiesConfig.getInstance().getProperties("padlocksCount")) {
                 finishMessage();
             }
         }
     }
-    
+
     public void tryUnlockTrack(String msj, int padlock) {
         if (padlocks.get(padlock).getRiddle().getTrackLock().tryUnlock((msj.toLowerCase().trim()))) {
             updateInfo(padlocks.get(padlock).getIdPadlock() + " pista desbloqueada\n");
             updateTracks(padlock);
         }
     }
-    
+
     public String getPadlockQuestion(int padlock) {
         return padlocks.get(padlock).getRiddle().getQuestion();
     }
-    
+
     public String getLockedTrackQuestion(int padlock) {
         return padlocks.get(padlock).getRiddle().getTrackLock().getQuestion();
     }
-    
+
     public String getTrackSimple(int padlock, int track) {
         return padlocks.get(padlock).getRiddle().getTracks().get(track);
     }
-    
+
     public String getUnlockTrack(int padlock) {
         return padlocks.get(padlock).getRiddle().getTrackLock().getTrack();
     }
-    
+
     private void loadPadlocks(RoomRiddle roomRiddle) {
         for (int i = 0; i < roomRiddle.getListRiddle().size(); i++) {
             padlocks.add(new Padlock(roomRiddle.getListRiddle().get(i), "Candado: " + (i + 1)));
         }
     }
-    
-    private void setPadlockText() {
-        for (int i = 0; i < padlocks.size(); i++) {
-            if (padlocks.get(i).isOpen()) {
-                padlocks.get(i).getRiddle().getTrackLock().setTrack(padlocks.get(i).getRiddle().getTrackLock().getTrack() + "\n" + padlocks.get(i).getIdPadlock());
-                for (int j = 0; j < padlocks.get(i).getRiddle().getTracks().size(); j++) {
-                    padlocks.get(i).getRiddle().getTracks().set(j, padlocks.get(i).getRiddle().getTracks().get(j) + "\n" + padlocks.get(i).getIdPadlock());
-                }
+
+    private void setPadlockText(int padlock) {
+        if (padlocks.get(padlock).isOpen()) {
+            padlocks.get(padlock).getRiddle().getTrackLock().setTrack(padlocks.get(padlock).getRiddle().getTrackLock().getTrack() + "\n" + padlocks.get(padlock).getIdPadlock());
+            for (int j = 0; j < padlocks.get(padlock).getRiddle().getTracks().size(); j++) {
+                padlocks.get(padlock).getRiddle().getTracks().set(j, padlocks.get(padlock).getRiddle().getTracks().get(j) + "\n" + padlocks.get(padlock).getIdPadlock());
             }
         }
     }
-    
+
     public void openWindowSingle() {
     }
-    
+
     private RoomInterface createWindows() {
         if (room instanceof Room1) {
             Room1 room1 = new Room1(this);
@@ -142,7 +140,7 @@ public class GameRoom extends Thread implements Subject {
         }
         return null;
     }
-    
+
     public void openWindowsMultiplayer(int players) {
         for (int i = 0; i < players; i++) {
             if (room instanceof Room1) {
@@ -150,35 +148,35 @@ public class GameRoom extends Thread implements Subject {
             }
         }
     }
-    
+
     @Override
     public void add(RoomInterface e) {
         rooms.add(e);
     }
-    
+
     @Override
     public RoomInterface remove(int index) {
         return rooms.remove(index);
     }
-    
+
     public void updateInfo(String msj) {
         for (int i = 0; i < rooms.size(); i++) {
             rooms.get(i).update(msj);
         }
     }
-    
+
     public void updateTracks(int padlock) {
         for (int i = 0; i < rooms.size(); i++) {
             rooms.get(i).unlockTrackLocked(padlock);
         }
     }
-    
+
     public void updatePadlock(int padlock) {
         for (int i = 0; i < rooms.size(); i++) {
             rooms.get(i).updatePadlock(padlock);
         }
     }
-    
+
     public void finishMessage() {
         String txt = "";
         if (!game.isFinishGame()) {
@@ -191,7 +189,7 @@ public class GameRoom extends Thread implements Subject {
             rooms.get(i).showMessageWin(txt);
         }
     }
-    
+
     public void deselectTeam() {
         for (int i = 0; i < subteam.size(); i++) {
             subteam.get(i).setSelected(false);
